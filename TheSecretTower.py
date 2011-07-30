186312c5d31486c5a5b5dfcddcb1ca0e09264dd5
pierre30994 <pierre.surply@gmail.com>
2011-07-30 17:07:36 +0200

    +GPL

--- TheSecretTower.py
+++ TheSecretTower.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python
-
 #-*-coding:Utf-8 -*
+
+#    TheSecretTower
+#    Copyright (C) 2011 Pierre SURPLY
+#
+#    This file is part of TheSecretTower.
+#
+#    TheSecretTower is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    TheSecretTower is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with TheSecretTower.  If not, see <http://www.gnu.org/licenses/>.
 
 ################################################
 #                                              #
